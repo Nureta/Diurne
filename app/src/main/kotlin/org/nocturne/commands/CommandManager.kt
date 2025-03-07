5095d07d3bbd3640b950c92b6ef0e308c6767a57
Meet P. <meetvk57@gmail.com>
2025-03-06 23:27:48 -0800

    Admin echo command example

--- app/src/main/kotlin/org/nocturne/commands/CommandManager.kt
+++ app/src/main/kotlin/org/nocturne/commands/CommandManager.kt
@@ -1,15 +1,20 @@
 package org.nocturne.commands
 
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.OptionMapping
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import java.util.ArrayList
+import java.util.function.Consumer
 
 object CommandManager {
+    var adminUsers = arrayOf("393982976125435934", "321419785189720064")
+
     private var hasInit = false
     private val commandMap = HashMap<String, MyCommand>()
     lateinit var mJda: JDA
@@ -21,8 +26,25 @@ object CommandManager {
         registerAllCommandMapCommands()
     }
 
+    /**
+     * Queue commands to be initialized here, they will be added to the command map.
+     */
     private fun initSimpleCommands() {
-        val simplePingCommand = MyCommand(
+        updateCommandMap(getAnimalCommand())
+        updateCommandMap(getEchoCommand())
+        updateCommandMap(getPokemonCommand())
+    }
+
+
+    // region Commands
+
+    /**
+     * Example command
+     * @see MyCommand
+     * Replies to whatever user responds with the same animal back.
+     */
+    private fun getAnimalCommand(): MyCommand {
+        return MyCommand(
             "animal",
             Commands.slash("animal", "Finds a random animal")
                 .addOptions(
@@ -35,15 +57,57 @@ object CommandManager {
         ) { event ->
             event.reply(event.getOption("type")!!.asString).queue() // reply immediately
         }
-
-        updateCommandMap(simplePingCommand)
     }
+
+    private fun getEchoCommand(): MyCommand {
+        // Echo Command
+        return MyCommand(
+            "adminecho",
+            Commands.slash("adminecho", "Echo!")
+                .addOption(OptionType.STRING, "msg", "What to Echo")
+        ) { event ->
+            val sender = event.member?.user?.id ?: return@MyCommand
+            if (!adminUsers.contains(sender)) return@MyCommand
+            val echoOpt = event.getOption("msg") ?: return@MyCommand
+
+            println("ECHO COMMAND - [${event.getChannel()}] ${event.member?.user?.name ?: "Unknown User"}: ${echoOpt}\n")
+            event.channel.sendMessage(echoOpt.asString).queue(object : Consumer<Message> {
+                override fun accept(msg: Message) {
+                    System.out.printf("Sent Message %s\n", msg);
+                    event.reply("Success").setEphemeral(true).queue()
+                }
+            })
+        }
+    }
+
+    private fun getPokemonCommand(): MyCommand {
+        return MyCommand(
+            "pokemon",
+            Commands.slash("pokemon", "Finds a random animal")
+                .addOptions(
+                    OptionData(OptionType.STRING, "type", "The type of animal to find")
+                        .addChoice("Pikachu", "Pika")
+                        .addChoice("MewTwo", "MewThree")
+                        .addChoice("Piplup", "piplup")
+                        .addChoice("Fish", "fish")
+                )
+        ) { event ->
+            event.reply(event.getOption("type")!!.asString).queue() // reply immediately
+        }
+    }
+
+
+    // endregion
+
 
     private fun updateCommandMap(command: MyCommand): Boolean {
         commandMap[command.commandName] = command
         return true
     }
 
+    /**
+     * Take command map [command name] -> [MyCommand], and register every command to discord.
+     */
     private fun registerAllCommandMapCommands() {
         val cmdData = ArrayList<CommandData>()
         for (cmd in commandMap.values) {
@@ -53,6 +117,9 @@ object CommandManager {
         mJda.updateCommands().addCommands(cmdData).queue()
     }
 
+    /**
+     * This will handle all command interactions and route them to the correct callback
+     */
     class GenericCommandListener : ListenerAdapter() {
         override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
             var cmd = commandMap[event.name]
@@ -62,6 +129,11 @@ object CommandManager {
     }
 }
 
+/**
+ * Command Name: The name the command to use
+ * Data: Specify command name same as name here, and options/parameters for this command
+ * CommandCallback: What to do when a user calls a command
+ */
 class MyCommand(
     var commandName: String,
     var data: CommandData,
--- app/src/main/kotlin/org/nocturne/listeners/OnMessageSentListener.kt
+++ app/src/main/kotlin/org/nocturne/listeners/OnMessageSentListener.kt
@@ -1,13 +1,13 @@
 package org.nocturne.listeners
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.internal.utils.JDALogger
 
 class OnMessageSentListener : ListenerAdapter() {
     override fun onMessageReceived(event: MessageReceivedEvent) {
         if (event.author.isBot) return
-        System.out.printf("[%s] %#s: %s\n",
-            event.getChannel(),
-            event.getAuthor(),
-            event.getMessage().getContentDisplay())
+        println(
+            "[${event.getChannel()}] ${event.author}: ${event.message.contentDisplay}\n",
+        )
     }
 }
